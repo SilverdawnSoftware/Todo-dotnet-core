1222293c9a02f6bbbc5d33129ab9044c989108e5
Daniel Pamich <daniel@silvadawn.com>
2018-12-14 11:49:57 +0000

    remove old code

--- ToDoWebClientold/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-﻿import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: ` <router-outlet></router-outlet>
-  <router-outlet name="popup"></router-outlet>`,
-})
-export class AppComponent  {  }
